1d61610e4ac05157afd70d9785afb37017a8125d
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2020-10-27 10:36:57 +0100

    Identity scaffolded

---
+++ Meeter.Website/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Meeter.Website.Server.Data;
+using Meeter.Website.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Meeter.Website.Server.Areas.Identity.IdentityHostingStartup))]
+namespace Meeter.Website.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
